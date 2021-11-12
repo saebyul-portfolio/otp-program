ecab13219feeffae5a63ef65ff3ffa336427bdea
saebyul1221 <dw696969@naver.com>
2021-11-13 02:18:37 +0900

    fix(post): wrong ContenTtype

--- otp-program/Form2.cs
+++ otp-program/Form2.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net.Http.Json;
 using Newtonsoft.Json;
-using System.Net.Json;
 using Newtonsoft.Json.Linq;
 using System.Net;
 using System.Web;
@@ -16,9 +15,9 @@ namespace otp_program
             InitializeComponent();
         }
 
-        // Body에 데이터를 못 보냄
         private void button1_Click(object sender, EventArgs e)
         {
+
             if (idBox.Text == "")
             {
                 MessageBox.Show("OTP 토큰을 입력해주세요.");
@@ -26,19 +25,17 @@ namespace otp_program
             }
             string url = @"http://localhost/check";
 
-
+            
             var json = "{\"token\": \"" + idBox.Text + "\",\"program\":\"IDK\"}";
-            dynamic data = JsonValue.Parse(json);
-
-            MessageBox.Show(data.ToString());
+            dynamic data = JsonNode.Parse(json);
 
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.ContentType = "application/json"; 
             httpWebRequest.Method = "POST";
 
             using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
             {
-                streamWriter.Write(data.ToString());
+                streamWriter.Write(data);
                 streamWriter.Flush();
                 streamWriter.Close();
                 try
